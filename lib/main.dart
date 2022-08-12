fca707eddfe1d9701c0855eefd52d0d19d0acb12
anasfik <ffikhi.aanas@gmail.com>
2022-08-12 11:32:09 +0100

    task completed, linked to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:kordofan_app/auth/login.dart';
 import 'package:kordofan_app/auth/signup.dart';
@@ -9,47 +6,40 @@ import 'package:kordofan_app/grouches/meeting.dart';
 import 'package:kordofan_app/home/homepage.dart';
 import 'package:kordofan_app/notifictions%20and%20result/notifications.dart';
 import 'package:kordofan_app/notifictions%20and%20result/result.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-
-void main() {
-  runApp(MyApp());
-
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
- 
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-        return  MaterialApp(
-          debugShowCheckedModeBanner: false,
-          
-          theme : ThemeData(
-            
-            primaryColor: Colors.indigo[100] ,
-            primarySwatch: Colors.indigo,
-            
-            
-            // buttonTheme: Color(Colors.blue),
-            // ignore: prefer_const_constructors
-            textTheme: TextTheme(
-              
-            ) ,
-            
-            ),
-        home:  Login(),
-        
-        routes: {
-          "login" : (context) =>   Login(),
-          "signup" : (context) => Signup(),
-          "homepage" : (context) => HomePage(),
-          "grouche" : (context) => Grouche(),
-          "meeting" : (context) => Meeting(),
-          "notifications": (context) =>  Notific(),
-          "resultz" : (context) => Resultz(),
-
-        },
-        );
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.indigo[100],
+        primarySwatch: Colors.indigo,
+        textTheme: const TextTheme(),
+      ),
+      home: Login(),
+      routes: {
+        "login": (context) => const Login(),
+        "signup": (context) => Signup(),
+        "homepage": (context) => HomePage(),
+        "grouche": (context) => Grouche(),
+        "meeting": (context) => Meeting(),
+        "notifications": (context) => Notific(),
+        "resultz": (context) => const Resultz(),
+      },
+    );
   }
 }
--- lib/notifictions and result/notifications.dart
+++ lib/notifictions and result/notifications.dart
@@ -1,56 +1,256 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unused_local_variable, must_be_immutable
 
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Notific extends StatelessWidget {
-  //  Notific({Key? key}) : super(key: key);
+class Notific extends StatefulWidget {
+  @override
+  State<Notific> createState() => _NotificState();
+}
+
+class _NotificState extends State<Notific> {
+  /// Variables
+
+  // list where we store the data from the firestore database
+  late List dataList;
+
+  // TextFields controllers to get value from TextFields to use them
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+
+  // firestore
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+// load contents from firestore
+  Future<List> getDataFromFirestore() async {
+    dataList = await firestore.collection('mainCollection').get().then((value) {
+      return value.docs.map((doc) => doc.data()).toList();
+    });
+
+    return dataList;
+  }
+
+// show dialog to put data
+  showInformationDialog() {
+    showDialog(
+        context: (context),
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              "عن الاعلان",
+              textAlign: TextAlign.right,
+            ),
+            content: Container(
+              width: MediaQuery.of(context).size.width * .8,
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        labelText: "عنوان",
+                      ),
+                    ),
+                    TextField(
+                      controller: descriptionController,
+                      decoration: InputDecoration(
+                        labelText: "وصف",
+                      ),
+                    ),
+                    TextField(
+                      controller: dateController,
+                      decoration: InputDecoration(
+                        labelText: "تاريخ",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextButton(
+                    child: Text("حفظ"),
+                    onPressed: () async {
+                      //call
+                      await addNewAd(
+                          titleController.text.trim(),
+                          descriptionController.text.trim(),
+                          dateController.text.trim());
+                    },
+                  ),
+                  TextButton(
+                    child: Text("الغاء"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ],
+          );
+        });
+  }
+
+//
+// add new ad to firestore
+  addNewAd(String title, String description, String date) async {
+    try {
+      await firestore.collection('mainCollection').add({
+        'title': title,
+        'description': description,
+        'date': date,
+        "createdAt": DateTime.now(),
+      });
+      // close dialog
+      Navigator.of(context).pop();
+
+      setState(() {});
+    } on FirebaseException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("${e.message}"),
+        ),
+      );
+    }
+  }
+
+// delete all ads from firestore
+  deleteAll() async {
+    FirebaseFirestore.instance.collection("mainCollection").get().then((value) {
+      value.docs.forEach((element) {
+        FirebaseFirestore.instance
+            .collection("mainCollection")
+            .doc(element.id)
+            .delete()
+            .then((value) {
+          print("Success!");
+          setState(() {});
+        });
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
-      child: Scaffold(
-        appBar:appBar() ,
-        drawer: Drawer(
-          child: Column(children: [
-              UserAccountsDrawerHeader(accountName: Text("كلية الاقتصاد و الدراسات التجارية"),
-               accountEmail: Text("kordofan.edu.sd" )),
-               ListTile(title:Text("الصفحة الرئيسية"),
-                leading: Icon(Icons.home),onTap: (){
+      child: RefreshIndicator(
+        onRefresh: () async {
+          await getDataFromFirestore();
+        },
+        child: Scaffold(
+          appBar: appBar(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              dataList = await getDataFromFirestore();
+
+              setState(() {});
+            },
+            child: Icon(Icons.sync),
+          ),
+          drawer: Drawer(
+              child: Column(
+            children: [
+              UserAccountsDrawerHeader(
+                  accountName: Text("كلية الاقتصاد و الدراسات التجارية"),
+                  accountEmail: Text("kordofan.edu.sd")),
+              ListTile(
+                title: Text("الصفحة الرئيسية"),
+                leading: Icon(Icons.home),
+                onTap: () {
                   Navigator.of(context).pushNamed("homepage");
-          
-               },),
-               ListTile(title:Text("FAQ"),
-                leading: Icon(Icons.face),onTap: (){
-          
-               },),
-               ListTile(title:Text("تسجيل الخروج"),
-                leading: Icon(Icons.home),onTap: (){
+                },
+              ),
+              ListTile(
+                title: Text("FAQ"),
+                leading: Icon(Icons.face),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text("تسجيل الخروج"),
+                leading: Icon(Icons.home),
+                onTap: () {
                   Navigator.of(context).pushReplacementNamed("login");
-          
-               },),
-          
-            ],)
+                },
+              ),
+            ],
+          )),
+          body: listView(),
         ),
-        body: listView(),
       ),
     );
   }
-  PreferredSizeWidget appBar(){
+
+  PreferredSizeWidget appBar() {
     return AppBar(
-      title: Text("لوحة الاعلانات"));
-  }
-  
-  Widget listView(){
-    return ListView.separated(
-      itemBuilder: (context, index){
-      return listViewItem(index);
-    }, separatorBuilder: (context, index){
-      return Divider(height: 0,);
-    }, itemCount: 15);
+      title: Text("لوحة الاعلانات"),
+      actions: [
+        IconButton(
+          onPressed: () async {
+            deleteAll();
+          },
+          icon: Icon(Icons.delete),
+        ),
+        IconButton(
+          onPressed: () async {
+            showInformationDialog();
+          },
+          icon: Icon(Icons.add),
+        ),
+      ],
+    );
   }
 
-  Widget listViewItem(int index){
+  Widget listView() {
+    return FutureBuilder(
+        future: getDataFromFirestore(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("حدث خطأ ما"),
+            );
+          }
+
+          if (snapshot.hasData && snapshot.data!.isEmpty) {
+            return Center(
+              child: Text("لا يوجد اعلانات"),
+            );
+          }
+          if (snapshot.hasData) {
+            return ListView.separated(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                return listViewItem(index);
+              },
+              separatorBuilder: (context, index) {
+                return Divider(
+                  height: 0,
+                );
+              },
+            );
+          }
+
+          return Center(
+            child: Text("something weird"),
+          );
+        });
+  }
+
+  Widget listViewItem(int index) {
     return Container(
       margin: EdgeInsets.only(left: 10),
       child: Row(
@@ -71,10 +271,10 @@ class Notific extends StatelessWidget {
           ),
         ],
       ),
-      
     );
   }
-  Widget prefixIcon(){
+
+  Widget prefixIcon() {
     return Container(
       height: 50,
       width: 50,
@@ -82,50 +282,58 @@ class Notific extends StatelessWidget {
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: Colors.grey.shade300,
-
       ),
-      child: Icon(Icons.notifications , size: 25 ,color: Colors.blue[300],),
+      child: Icon(
+        Icons.notifications,
+        size: 25,
+        color: Colors.blue[300],
+      ),
     );
   }
-  Widget message(int index){
+
+  Widget message(int index) {
     double textSize = 20;
-    return Container(
-      child: RichText(maxLines: 4,
+    return RichText(
+      maxLines: 4,
       overflow: TextOverflow.ellipsis,
       text: TextSpan(
-        text: " الاعلان ",
+        text: "${dataList[index]['title']} ",
         style: TextStyle(
-          fontSize: textSize,
-          color: Colors.black,
-          fontWeight: FontWeight.bold
-        ),
+            fontSize: textSize,
+            color: Colors.black,
+            fontWeight: FontWeight.bold),
         children: [
           TextSpan(
-            text: "محتوى الاعلان ",
+            text: dataList[index]['description'],
             style: TextStyle(
               fontWeight: FontWeight.w400,
             ),
           )
         ],
       ),
-      ),
     );
   }
-  Widget timeAndDate(int index){
+
+  Widget timeAndDate(int index) {
     return Container(
       margin: EdgeInsets.only(top: 5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("10.8.2022", style: TextStyle(
-            fontSize: 10,
-          ),),
-           Text("3.8.2022", style: TextStyle(
-            fontSize: 10,
-          ),),
+          Text(
+            dataList[index]['date'],
+            style: TextStyle(
+              fontSize: 10,
+            ),
+          ),
+          Text(
+            dataList[index]['date'],
+            style: TextStyle(
+              fontSize: 10,
+            ),
+          ),
         ],
       ),
     );
-
   }
 }
